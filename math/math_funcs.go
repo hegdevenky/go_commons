724a91f7a8b14aab493e2c82400778e6481ac755
Venky Shantharam Hegde <venky.shantharam.hegde@oracle.com>
2025-02-07 13:31:45 -0600

    Adding PercentageOf function to math package. Updated comments and test cases.

--- math/math_funcs.go
+++ math/math_funcs.go
@@ -1,10 +1,23 @@
 package math
 
-import "golang.org/x/exp/constraints"
+import (
+	"golang.org/x/exp/constraints"
+)
 
+// Abs a generic function that returns the absolute value of a given signed number.
+// Works on both signed & unsigned integers and floating point numbers.
 func Abs[T constraints.Integer | constraints.Float](val T) T {
 	if val < 0 {
 		return -val
 	}
 	return val
+}
+
+// PercentageOf a generic function to calculate the percentage of a given number
+// Returned result will be of type float64.
+// Usage examples:
+//   - to calculate 3% of 91 => PercentageOf(3, 91) or  PercentageOf[int](3, 91)
+//   - to calculate 2.5% of 68.65 => PercentageOf(2.5, 68.65) or PercentageOf[float32](2.5, 68.65)
+func PercentageOf[T constraints.Integer | constraints.Float](p, x T) float64 {
+	return (float64(p) / 100) * float64(x)
 }
--- math/math_funcs_test.go
+++ math/math_funcs_test.go
@@ -1,6 +1,7 @@
 package math
 
 import (
+	"golang.org/x/exp/constraints"
 	"testing"
 )
 
@@ -39,6 +40,55 @@ func TestAbs(t *testing.T) {
 			ans := Abs(test.input)
 			if ans != test.want {
 				t.Errorf("got %f, want %f", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestPercentageOf(t *testing.T) {
+	type args[T interface {
+		constraints.Integer | constraints.Float
+	}] struct {
+		p T
+		x T
+	}
+	type testCase[T interface {
+		constraints.Integer | constraints.Float
+	}] struct {
+		name string
+		args args[T]
+		want float64
+	}
+
+	// testing float64
+	tests := []testCase[float64]{
+		{"test1", args[float64]{3, 91}, 2.73},
+		{"test2", args[float64]{5, 91}, 4.55},
+		{"test3", args[float64]{0, 91}, 0},
+		{"test4", args[float64]{7, 0}, 0},
+		{"test5", args[float64]{0, 0}, 0},
+		{"test6", args[float64]{2.5, 68.65}, 1.7162500000000003},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PercentageOf(tt.args.p, tt.args.x); got != tt.want {
+				t.Errorf("PercentageOf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	// testing int
+	testInts := []testCase[int]{
+		{"test1", args[int]{3, 91}, 2.73},
+		{"test2", args[int]{5, 91}, 4.55},
+		{"test3", args[int]{0, 91}, 0},
+		{"test4", args[int]{7, 0}, 0},
+		{"test5", args[int]{0, 0}, 0},
+	}
+	for _, tt := range testInts {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PercentageOf(tt.args.p, tt.args.x); got != tt.want {
+				t.Errorf("PercentageOf() = %v, want %v", got, tt.want)
 			}
 		})
 	}
